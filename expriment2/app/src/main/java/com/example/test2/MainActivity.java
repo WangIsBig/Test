74cfc790b73fa507782ad42da113c9eaf1552303
WangIsBig <350409499@qq.com>
2025-12-02 10:51:25 +0800

    experiment2

---
+++ expriment2/app/src/main/java/com/example/test2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.test2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 加载你的XML布局文件
+        setContentView(R.layout.constraintlayout_2);
+    }
+}
